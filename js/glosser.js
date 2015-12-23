b18ad5e9f25e32dba5cc6b973ef03556c59a9e2d
Ico Davids <ico.davids@gmail.com>
2015-12-23 17:42:07 +0100

    Added options
    Separator character, empty gloss character, and escape character for
    gloss groups

--- js/glosser.js
+++ js/glosser.js
@@ -1,31 +1,46 @@
 jQuery.fn.extend({
-	glosser: function() {
-		this.each(function() {
-			var container = $(this);
-			var content = container;
-			content = $.trim(content.html());
-			var lines = content.split('\n');
-			var glosses = $('div');
-			container.empty().addClass('glosser-container');
-			$.each(lines, function(i) {
-				lines[i] = $.trim(lines[i]);
-				lines[i] = lines[i].replace(/  +/g, ' ');
-				lines[i] = lines[i].replace(/{(.*?)}/g, function(s) {
-					s = s.replace(/[\s]/g, '&nbsp;');
-					s = s.replace(/[{|}]/g, '');
-					return s;
-				});
-				lines[i] = lines[i].split(' ');
-			});
+  glosser: function(options) {
 
-			for (var i = 0; i < lines[0].length; i++) {
-				var gloss = $('<span>').addClass('glosser-item');
-				gloss.append('<ul>');
-				$.each(lines, function(j) {
-					gloss.find('ul').append('<li class="glosser-row-' + (j + 1) + '">' + lines[j][i] + '</li>');
-				});
-				container.append(gloss);
-			}
-		});
-	}
+    var defaults = {
+      separator:  " ",
+      emptyGloss: "",
+      escape:     "{}"
+    }
+    var settings = $.extend({}, defaults, options);
+    
+    return this.each(function() {        
+      var container = $(this);
+      var content = container;
+      content = $.trim(content.html());
+      var lines = content.split('\n');
+      var glosses = $('div');
+      container.empty().addClass('glosser-container');
+      $.each(lines, function(i) {
+        lines[i] = $.trim(lines[i]);
+        lines[i] = lines[i].replace(/  +/g, ' ');
+        var escapeChars = settings.escape.split('');
+        // Look for escape characters
+        var escape_regex = new RegExp('\\'+escapeChars[0]+"(.*?)\\"+escapeChars[1], 'g');
+        lines[i] = lines[i].replace(escape_regex, function(s) {
+          // Replace spaces by non-breaking space 
+          s = s.replace(/ /g, '&nbsp;');
+          // Remove escape characters from string
+          var escape_regex2 = new RegExp('[\\'+ (settings.escape.split('')).join('|\\') +']','g');
+          s = s.replace(escape_regex2, '');
+          return s;
+        });
+        // Split line into gloss items, separated by separator
+        lines[i] = lines[i].split(settings.separator);
+      });
+
+      for (var i = 0; i < lines[0].length; i++) {
+        var gloss = $('<span><ul></ul></span>').addClass('glosser-item');
+        $.each(lines, function(j) {
+          var glossWord = (typeof lines[j][i] !== "undefined") ? lines[j][i] : "";
+          gloss.find('ul').append('<li class="glosser-row-' + (j + 1) + '">' + glossWord + '</li>');
+        });
+        container.append(gloss);
+      }
+    });
+  }
 });
